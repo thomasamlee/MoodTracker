ebbeb5fcd4ad0245e4e2f27f031c6c1fc8cbbf9b
Thomas Amlee <thomasamlee@thomass-air.lan>
2023-04-24 11:08:30 -0500

    store moods in local storage

--- src/App.provider.tsx
+++ src/App.provider.tsx
@@ -1,18 +1,62 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, {PropsWithChildren} from 'react';
 import {MoodOptionWithTimestamp, MoodOptionType} from './types';
+
+const storageKey = 'my-app-data';
+
+type AppData = {
+  moods: MoodOptionWithTimestamp[];
+};
+
+const getAppData = async (): Promise<AppData | null> => {
+  try {
+    const data = await AsyncStorage.getItem(storageKey);
+
+    if (data) {
+      return JSON.parse(data);
+    }
+    return null;
+  } catch {
+    return null;
+  }
+};
+
+const setAppData = async (newData: AppData) => {
+  try {
+    await AsyncStorage.setItem(storageKey, JSON.stringify(newData));
+  } catch {}
+};
 
 type AppContextType = {
   moodList: MoodOptionWithTimestamp[];
   handleSelectMood: (mood: MoodOptionType) => void;
 };
 
-const AppContext = React.createContext<AppContextType>([]);
+const AppContext = React.createContext<AppContextType>({
+  moodList: [],
+  handleSelectMood: () => {},
+});
 
 export const AppProvider: React.FC<PropsWithChildren> = ({children}) => {
   const [moodList, setMoodList] = React.useState<MoodOptionWithTimestamp[]>([]);
 
+  React.useEffect(() => {
+    const getDataFromStorage = async () => {
+      const data = await getAppData();
+
+      if (data) {
+        setMoodList(data.moods);
+      }
+    };
+    getDataFromStorage();
+  }, []);
+
   const handleSelectMood = React.useCallback((mood: MoodOptionType) => {
-    setMoodList(current => [...current, {mood, timestamp: Date.now()}]);
+    setMoodList(current => {
+      const newValue = [...current, {mood, timestamp: Date.now()}];
+      setAppData({moods: newValue});
+      return newValue;
+    });
   }, []);
 
   return (
